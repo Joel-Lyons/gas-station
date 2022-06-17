2104d778d8ff28860fdee4c718f184586ff79382
Joel Lyons <joel.lyons@outlook.com>
2022-06-16 19:09:16 -0500

    ~

--- gas_station.py
+++ gas_station.py
@@ -8,20 +8,20 @@ class Solution(object):
         start_position = 0
         gas_tank = 0
         n = len(gas)
-        i = start_position
+        i = 0
+
         while start_position < n:
             gas_amt = gas[i]
             cost_amt = cost[i]
             gas_tank += gas_amt
-            if i + 1 < n:
-                i += 1
-            else:
-                i = 0
-            if gas_tank < cost_amt:
-                gas_tank = 0
-                start_position += 1
-            else:
+
+            if gas_tank >= cost_amt:
+                gas_tank -= cost_amt
+                i = 0 if i + 1 >= n else i + 1
                 if start_position == i:
                     return i
-                gas_tank -= cost_amt
+            else:
+                gas_tank = 0
+                start_position += 1
+                i = start_position
         return -1
